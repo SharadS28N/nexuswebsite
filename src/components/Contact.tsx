4d9414c2d0b6aac94366dc6471808d0121cd0732
forgoto69 <85154812+forgoto69@users.noreply.github.com>
2025-05-21 21:01:23 +0545

    Add EmailJS and implement email functionality

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -32,7 +32,7 @@ const Contact = () => {
     
     try {
       await emailjs.sendForm(
-        'YOUR_SERVICE_ID', // Replace with your EmailJS service ID
+        'Yservice_c0sbozr', // Replace with your EmailJS service ID
         'YOUR_TEMPLATE_ID', // Replace with your EmailJS template ID
         formRef.current,
         'YOUR_PUBLIC_KEY' // Replace with your EmailJS public key
